592d3b0aa2b189e4fc5ff04dabf66e89e82bc374
Aleksandr Aravkin <sasha.aravkin@gmail.com>
2013-05-21 16:24:58 -0400

    compare results using minFunc.

---
+++ GlobalExamples/Student.m
@@ -0,0 +1,64 @@
+% Written by Aleksandr Aravkin (saravkin at us dot ibm dot com)
+% L1-regularized Huber example. 
+%clear all;
+%Generate problem data
+randn('seed', 0);
+rand('seed',0);
+
+
+% choose which ADMM program to run; whether to 
+% to use smooth version (1, runs huberl1smooth) 
+% or nonsmooth (2, runs huberl1)
+smooth = 0;
+
+% Choose whether the problem is well conditioned (1) or ill-conditioned (0)
+wellCond = 1;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+m = 200;       % number of examples
+n = 50;       % number of features
+
+p = 1;      % sparsity density
+
+x0 = randn(n, 1);
+A = randn(m,n);
+
+b = A*x0 + sqrt(0.001)*randn(m,1);
+%b = b + 10*sprand(m,1,300/m);      % add sparse, large noise
+
+params.procLinear = 0;
+
+%Solve problem
+
+
+params.meas_kappa = 1;
+params.uConstraints = 1;
+params.meas_scale = 2;
+params.uMax = params.meas_scale;
+params.uMin = -params.meas_scale;
+
+
+boxSize = 2;
+params.constraints = 0; 
+Con = [speye(n); -speye(n)];
+params.a = boxSize*ones(2*n, 1);
+params.A = Con';
+
+
+[xIP] = run_example(A, b, 'student', [], params);
+%Reporting
+
+prm.df = params.meas_scale;
+prm.b = b;
+prm.A = A;
+mFun = @(x)students(x, prm);
+xMF = minFunc(mFun, zeros(n,1));
+
+
+fprintf('Our objective: %5.3f, minFunc objective: %5.3f\n', mFun(xIP), mFun(xMF));
+
+
+ fprintf('Inf norm between our solution and minFunc solution: %5.3f\n', norm(xMF - xIP, inf));
+%[xIP xCVX]
