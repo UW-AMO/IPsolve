4a27a080e8cbe15f2d918e226575aba9729e6572
Aleksandr Aravkin <sasha.aravkin@gmail.com>
2016-05-02 09:49:33 -0400

    updated kalman demo with exp and also constraints

--- 515Examples/KalmanDemo.m
+++ 515Examples/KalmanDemo.m
@@ -1,70 +1,111 @@
-%% Kalman example: exploiting matrix structure! 
+%% Kalman example: exploiting matrix structure!
 
 clear all; close all; clc
 
 signal = 'exp';
-%
-
 switch(signal)
-    case{'exp'}
-        signalFunc = @(x) exp(sin(8*x)); % my favorite
-        numP  = 1;         % number of periods
+    
     case{'sine'}
-        signalFunc = @(x) sin(-x); % my favorite
+        gamma =  1;         % transition covariance multiplier
+        signalFunc = @(x) sin(-x);
+        box = [-1,1]; % lower, upper 
         numP = 4;
+        N     = 100;        % number of measurement time points
+        dt    = numP*2*pi / N;  % time between measurement points
+        sigma =  .4;       % standard deviation of measurement noise
+        sigmaMod = 0.1;    % sigma we tell smoother
+        outliers = 0;
+        if(outliers)
+            out = N*.05;        % percent of outliers
+            mag = 5;            % outlier variance
+        else
+            out = 0;
+            mag = 0;
+        end
+        
+        % box constraints for constrained estimates
+        conA = [0 1; 0 -1];
+        cona = [1; 1];
+        
+        measPLQ = 'huber';
+        procPLQ = 'l2';
+        
+        
+    case{'exp'}
+        gamma = 0.1;
+        mult = 4;
+        signalFunc = @(x) exp(sin(mult*x));
+        numP  = mult;         %
+        N     = 100;        % number of measurement time points
+        dt    = numP*2*pi / (N*mult);  % time between measurement points
+        sigma =  .05;       % standard deviation of measurement noise
+        sigmaMod = sigma;   % give nominal variance 
+        outliers = 0;
+        if(outliers)
+            out = N*.1;        % percent of outliers
+            mag = 5;            % outlier variance
+        else
+            out = 0;
+            mag = 0;
+        end
+        
+        
+        measPLQ = 'huber';
+        procPLQ = 'huber';
+        
+                % box constraints
+        conA = [0 1; 0 -1];
+        cona = [exp(1); -exp(-1)];
+
+        
+        
     otherwise
         error('unknown signal');
 end
 
-N     = 300;        % number of measurement time points
-dt    = numP*2*pi / N;  % time between measurement points
-gamma =  1;        % transition covariance multiplier
-sigma =  .05;       % standard deviation of measurement noise
+m     = 1;          % number of measurements per time point
+n     = 2;          % number of measurements per time point
+t       =  (1 : N) * dt; % set of times
+
+
 max_itr = 30;      % maximum number of iterations
 epsilon = 1e-5;    % convergence criteria
-h_min   = 0;       % minimum horizontal value in plots
-h_max   = dt*N;       % maximum horizontal value in plots
 % ---------------------------------------------------------
 
-%% Generate state data 
-% smooth signal: 
+
+
+%% Generate state data
+% smooth signal:
 
 rand('seed', 1234);
-% number of constraints per time point
-ell   = 4;
-% number of measurements per time point
-m     = 1;
-% number of state vector components per time point
-n     = 2;
-% simulate the true trajectory and measurement noise
-t       =  (1 : N) * dt;
 %x1_true = - cos(t);
-x_true = - signalFunc(t);
+x_true = signalFunc(t);
 
-v_min   = min(x_true)*1.3;    % minimum vertical value in plots
+% plotting parameters
+h_min   = 0;       % minimum horizontal value in plots
+h_max   = dt*N;       % maximum horizontal value in plots
+v_min   = min(x_true)*1.3 -.2;    % minimum vertical value in plots
 v_max   = max(x_true)*1.3 +.2;    % maximum vertical value in plots
 
 %x_true  = [ x1_true ; x2_true ];
 %
 % measurement values and model
 gaussErrors  = sigma * randn(1, N);
-out = N*.05;  % percent of outliers 
-mag = 1;   % outlier variance
 % construct the outliers
 outliers = zeros(1,N);
 inds = randperm(N, out); % indices of outliers
-outliers(inds) = mag*randn(1,out); 
+outliers(inds) = mag*randn(1,out);
 z       = x_true + gaussErrors + outliers;
 
 
-%% measurement model 
-rk      = (sigma * sigma);
+%% measurement model
+rk      = (sigmaMod * sigmaMod);
 rinvk   = 1 / rk;
 
 % create measurement model
 rh = sqrt(rinvk)*[0, 1];
-Hmat = kron(speye(N), rh); 
-meas = sqrt(rinvk)*z'; % scaling everyone 
+Hmat = kron(speye(N), rh);
+meas = sqrt(rinvk)*z'; % scaling everyone
 
 
 %% process model
@@ -76,7 +117,7 @@ gk = multMat*[ 1 , 0 ; dt , 1 ];
 Gmat = blktridiag(multMat*speye(n),-gk, zeros(n),N);
 w = zeros(n*N, 1);
 w(1:n) = 10*x_true(:,1);
-Gmat(1:n, 1:n) = 10*eye(n);
+Gmat(1:n, 1:n) = 10*eye(n); % initial state estimate 
 params.procLinear = 1;
 params.K = Gmat;
 params.k = w;
@@ -89,43 +130,72 @@ tic
 [ yOut ~] = run_example( Hmat, meas, 'l2', 'l2', [], params );
 toc
 
-xOut = reshape(yOut, n, N);
+xOut = reshape(yOut, n, N); % nominal estimate
 
 
-
-% Robust Kalman smoother - the code will automatically plot it.  
+% Robust Kalman smoother - the code will automatically plot it.
+% Huber huber!!
 tic
-[ yOut ~] = run_example( Hmat, meas, 'huber', 'huber', [], params );
+[ yOut ~] = run_example( Hmat, meas, measPLQ, procPLQ, [], params );
 toc
 
 xOutRobust = reshape(yOut, n, N);
 
 
+params.constraints = 1;
+A = kron(speye(N), conA);
+a = kron(ones(N,1), cona);
+params.A = A'; % historical artifact
+params.a = a; 
+
+tic
+[ yOut ~] = run_example( Hmat, meas, 'l2', 'l2', [], params );
+toc
+xOutCon = reshape(yOut, n, N); % constrained estimate
+
+tic
+[ yOut ~] = run_example( Hmat, meas, measPLQ, procPLQ, [], params );
+toc
+
+xOutConRobust = reshape(yOut, n, N);
 
 
 
 % if draw_plot
-    figure(1);
-    clf
-    hold on
-    plot(t', x_true', 'r-', 'Linewidth',2 );
-    plot(t', z(1,:)', 'ko', 'Linewidth', 2);
-    plot(t', xOut(2,:)', 'b--', 'Linewidth', 2);
-    plot(t', xOutRobust(2,:)', 'm-.', 'Linewidth',2 );
+figure(1);
+clf
+hold on
+plot(t', x_true', 'r-', 'Linewidth',2 );
+plot(t', z(1,:)', 'ko', 'Linewidth', 2);
+plot(t', xOut(2,:)', 'b--', 'Linewidth', 2);
+plot(t', xOutRobust(2,:)', 'm-.', 'Linewidth',2 );
 
-    %     plot(t', - ones(N,1), 'b-');
-    %      plot(t', ones(N,1), 'b-');
-    axis([h_min, h_max, v_min, v_max]);
-    title('Affine smoother');
-    legend('Truth', 'Measurements', 'Estimate', 'robust estimate', 'Location', 'SouthEast' );
-    hold off
-    %
-    % constrained estimate
-    x_con = xOut;
-    
-    
-    
-    
+%     plot(t', - ones(N,1), 'b-');
+%      plot(t', ones(N,1), 'b-');
+axis([h_min, h_max, v_min, v_max]);
+title('Unconstrained Estimates');
+legend('Truth', 'Meas.', 'Least Squares', 'Robust', 'Location', 'SouthEast' );
+hold off
+%
+
+figure(2);
+clf
+hold on
+plot(t', x_true', 'r-', 'Linewidth',2 );
+plot(t', z(1,:)', 'ko', 'Linewidth', 2);
+plot(t', xOutCon(2,:)', 'b--', 'Linewidth', 2);
+plot(t', xOutConRobust(2,:)', 'm-.', 'Linewidth',2 );
+
+%     plot(t', - ones(N,1), 'b-');
+%      plot(t', ones(N,1), 'b-');
+axis([h_min, h_max, v_min, v_max]);
+title('Constrained Estimates');
+legend('Truth', 'Measurements', 'Estimate', 'robust estimate', 'Location', 'SouthEast' );
+hold off
+%
+
+
+
 % end
 %    savefig(1, [pwd '/Experiments/affine']);
 %
