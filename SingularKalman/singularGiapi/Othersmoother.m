15f7a738568f912c952f526f0a120d9cc44c2c79
Aleksandr Aravkin <sasha.aravkin@gmail.com>
2017-01-26 07:33:09 -0800

    changed fix file

--- SingularKalman/singularGiapi/Othersmoother.m
+++ SingularKalman/singularGiapi/Othersmoother.m
@@ -1,16 +1,17 @@
 %Testing other singular smoother
 
 %%%%%%SINE EXAMPLE%%%%%
-
+addpath(genpath(pwd))
+clear all
 gamma = 1;
 signalFunc = @(x) sin(x);
 box = [-1,1]; % lower, upper 
 numP = 4;
-N     = 20;        % number of measurement time points
+N     = 40;        % number of measurement time points
 dt    = numP*2*pi / N;  % time between measurement points
 sigma =  .1;       % standard deviation of measurement noise
 sigmaMod = 0.1;    % sigma we tell smoot
-outliers = 1;
+outliers = 0;
 
 if(outliers)
     out = N*.05;        % percent of outliers
@@ -37,7 +38,7 @@ outliers = zeros(1,N);
 inds = randperm(N, out); % indices of outliers
 outliers(inds) = mag*randn(1,out);
 z       = x_true + gaussErrors + outliers;
-
+%z = z +100*rand(size(z));
 
 y = num2cell(z);
 Rvect = sigma^2*ones(1,N);
@@ -82,8 +83,11 @@ for i=1:n
     %%%%%%%%%%%%%%% MEASUREMENT UPDATE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     Ve{i}=h{i}*Pp{i}*h{i}'+R{i};%innovation variance
     e{i}=y{i}-h{i}*Xp(:,i);%innovation
-    Xf(:,i)=Xp(:,i)+Pp{i}*h{i}'*pinv(Ve{i})*e{i};
-    Pf{i}=Pp{i}-Pp{i}*h{i}'*pinv(Ve{i})*h{i}*Pp{i};
+    %Xf(:,i)=Xp(:,i)+Pp{i}*h{i}'*pinv(Ve{i})*e{i};
+    Xf(:,i)=Xp(:,i)+Pp{i}*h{i}'*(Ve{i}\e{i});
+    %Pf{i}=Pp{i}-Pp{i}*h{i}'*pinv(Ve{i})*h{i}*Pp{i};
+    Pf{i}=Pp{i}-Pp{i}*h{i}'*(Ve{i}\h{i})*Pp{i};
+
     %%%%%%%%%%%%%%%%%%% TIME UPDATE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     if i<n
         Xp(:,i+1)=g{i}*Xf(:,i);
@@ -100,22 +104,27 @@ Xs(:,n)=Xf(:,n);
 Ps{n}=Pf{n};
 for j=1:(n-1)
     i=n-j;
-    A{i}=Pf{i}*g{i}'*pinv(Pp{i+1});
+    %A{i}=Pf{i}*g{i}'*pinv(Pp{i+1});
+    A{i}=((Pp{i+1}\g{i})*Pf{i})'; % assumes Pp are symmetric
     Xs(:,i)=Xf(:,i)+A{i}*(Xs(:,i+1)-Xp(:,i+1));
     Ps{i}=Pf{i}+A{i}*(Ps{i+1}-Pp{i+1})*A{i}';
 end
 
-solutionps = PseudoSmoother(ginst, [1 0], z', Q{1}, R{1}, 2, 1, N, [0;1], 'l2');
-stateps = reshape(solutionps, 2, N);
+%solutionps = PseudoSmoother(ginst, [1 0], z', Q{1}, R{1}, 2, 1, N, [0;1], 'l2');
+%stateps = reshape(solutionps, 2, N);
 %[M, P] = qr(Q{1});
 M = Q{1};
 Proj = 1/(.25*dt^4+dt^2)*gain*gain';
 for k =1:n-1
-    check(k) = norm((eye(2,2) - Proj)*(Xf(:,k+1) - ginst*Xf(:,k)));
-    checkps(k) = norm((eye(2,2) - Proj)*(stateps(k+1) - ginst*stateps(k)));
+    check_f(k) = norm((eye(2) - Proj)*(Xf(:,k+1) - ginst*Xf(:,k)));
+    check_s(k) = norm((eye(2) - Proj)*(Xs(:,k+1) - ginst*Xs(:,k)));
+    %checkps(k) = norm((eye(2,2) - Proj)*(stateps(k+1) - ginst*stateps(k)));
 end
- plot(1:n-1, check, 'r', 1:n-1, checkps, 'k')
- legend('Giapi projected error', 'Pseudo solver projected error')
+% plot(1:n-1, check, 'r', 1:n-1, checkps, 'k')
+ plot(1:n-1, check_f, 'r')
+ hold on
+ plot(1:n-1, check_s, 'b')
+legend('Giapi projected filter error', 'Giapi projected smoother error')
 
 
 
